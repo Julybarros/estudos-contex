762adc539354d894ebd6f9445d0a6446a5c798ab
julybarros <julianajulyy22@gmail.com>
2023-03-23 09:59:15 -0300

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
+import { Propriedades } from "./components/Propriedades/Propriedades";
+
 export function App() {
-  return <div></div>;
+  return <div>
+   
+  </div>;
 }
+
+/** <Propriedades
+      cidade="Ubajara"
+      estado="CE"
+      preco={9}
+      ligado={false}
+      info={[1, 2, 3]}
+      objeto={{ nome: "Almir", idade: 19 }}
+    ></Propriedades>
+    <hr />
+    <Propriedades
+      cidade="Tianguá"
+      estado="CE"
+      preco={10}
+      ligado={true}
+      info={[1, 100]}
+      objeto={{ nome: "Almir", idade: 30 }}
+    ></Propriedades> */
